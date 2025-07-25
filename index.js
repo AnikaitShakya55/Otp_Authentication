db9fc4aab6f73be5ce5fbe7d1ad49ba351d6d166
Anikait Shakya <anikait.shakya@easexpense.com>
2025-07-25 11:07:36 +0530

    add test route

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/home", (req, res) => {
+  res.send("Welcome to Home!");
+});
 app.use("/auth", require("./routes/routes"));
 
 databaseConnection().then(() => {
