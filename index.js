920ba3ef7cf826f16ae16420d52d5947481c0b61
Anikait Shakya <anikait.shakya@easexpense.com>
2025-07-25 11:01:01 +0530

    commit 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const databaseConnection = require("./db/databaseConnection");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", require("./routes/routes"));
+
+databaseConnection().then(() => {
+  app.listen(process.env.BACKEND_PORT, () => {
+    console.log(`SERVER IS LISTENING AT ${process.env.BACKEND_PORT}`);
+  });
+});
